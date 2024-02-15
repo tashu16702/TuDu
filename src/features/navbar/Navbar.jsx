b10b4ba8c260097831a81888b50a51c5f0321f6b
tashu16702 <tashu16702@gmail.com>
2024-02-15 14:35:07 +0530

    15-02-24

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -1,50 +1,58 @@
 import React from 'react'
 
+
 export default function Navbar() {
   return (
     <div>
-      <div className="navbar bg-base-100">
-  <div className="navbar-start">
+      <div className="navbar bg-base-100" style={{color:'white'}}>
+  <div className="navbar-start" >
+    {/* dropdown of navbar */}
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <li><a>Home</a></li>
+      <li><a>Create</a></li>
+      <li><a>Booking</a></li>
+      <li><a>About</a></li>
+      <li><a>Contact</a></li>
       </ul>
     </div>
+    {/* logo */}
     <a className="btn btn-ghost text-xl">
-  <img src="tudu_logo.png" alt="Image Description" />
+  <img src="./tudu_logo.png" alt="Image Description" style={{  width: '100px', height: '55px ' }}/>
 </a>
 
   </div>
-  <div className="navbar-center hidden lg:flex">
+{/* navbar and signin */}
+  <div className="navbar-end">
+    {/* navbar  */}
+  <div className="hidden lg:flex ">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      <li ><a>Home</a></li>
+      <li><a>Create</a></li>
+      <li><a>Booking</a></li>
+      <li><a>About</a></li>
+      <li><a>Contact</a></li>
     </ul>
   </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn" style={{color:'white'}}>Sign In</a>
   </div>
 </div>
+
+<div className="hero h-30" style={{backgroundImage: 'url(./head_bg.jpg)' }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+
+    <div className="max-w-md">
+      <h1 className="mb-5 text-5xl font-bold" style={{color:'white'}}>TuDu Flow Dreams Begin To Glow</h1>
+      <h1 className="mb-2 text-5sl font-bold" style={{color:'white'}}>Event ? When ? Where ?</h1>
+    </div>
+  </div>
+</div>
+
+
     </div>
   )
 }
